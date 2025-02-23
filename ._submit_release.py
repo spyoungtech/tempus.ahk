6e31fe6ebae9141e698491e6f748fafa63120e28
Spencer Phillip Young <spencer.young@spyoung.com>
2025-02-22 20:30:21 -0800

    add hashing and virus total to release process

---
+++ ._submit_release.py
@@ -0,0 +1,56 @@
+import os
+import requests
+import hashlib
+
+
+virus_total_api_key = os.getenv("VIRUS_TOTAL_KEY")
+
+
+url = "https://www.virustotal.com/api/v3/files"
+
+files = {
+    "file": (
+        "tempus_ahk.dll",
+        open("dist\\tempus_ahk.dll", "rb"),
+        "application/octet-stream",
+    )
+}
+headers = {"accept": "application/json", "x-apikey": virus_total_api_key}
+
+response = requests.post(url, files=files, headers=headers)
+
+response.raise_for_status()
+
+data = response.json()
+info_url = data["data"]["links"]["self"]
+
+
+info_response = requests.get(info_url, headers=headers)
+info_response.raise_for_status()
+info_data = info_response.json()
+vt_sha_256 = info_data["meta"]["file_info"]["sha256"]
+
+
+for fname in os.listdir("dist"):
+    fp = os.path.join("dist", fname)
+    sha_256 = hashlib.sha256()
+    md5 = hashlib.md5()
+    with open(fp, "rb") as f:
+        contents = f.read()
+    sha_256.update(contents)
+    md5.update(contents)
+    sha256_hash = sha_256.hexdigest()
+    md5_hash = md5.hexdigest()
+    md5_text = fp + ".md5.txt"
+    sha256_text = fp + ".sha256.txt"
+
+    with open(md5_text, "w", encoding="utf-8") as f:
+        f.write(md5_hash)
+
+    with open(sha256_text, "w", encoding="utf-8") as f:
+        f.write(sha256_hash)
+    print()
+    print(f"- {fname} SHA256: {sha256_hash}")
+    print(f"- {fname} MD5: {md5_hash}")
+
+print(f"\n\n[virus total link](https://www.virustotal.com/gui/file/{vt_sha_256}")
