a0120b6e13fbff0730b639fa9c915800ad8c103a
Spencer Young <spencer.young@spyoung.com>
2025-02-27 11:12:54 -0800

    add relative to test for span compare

--- src/tests.rs
+++ src/tests.rs
@@ -283,6 +283,17 @@ fn test_span_compare_24_hours() {
     assert!(output.status.success());
 }
 
+#[test]
+fn test_span_compare_relative() {
+    let script = make_script("span1 := Span.new().days(30)\nspan2 := Span.new().weeks(3)\nrelative_to := DateTime.now()\nwritestdout(span1.gt(span2, relative_to))");
+    let output = run_script(script);
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    assert_eq!(stderr, "");
+    assert_eq!(stdout.to_string(), String::from("1"));
+    assert!(output.status.success());
+}
+
 
 #[test]
 fn test_span_total() {
